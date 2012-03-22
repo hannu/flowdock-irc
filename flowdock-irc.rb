5f1b6abc897fcc091d03eef9e860c2a4692f5912
Hannu Pelkonen <hannup@gmail.com>
2012-03-22 21:47:29 +0200

    Woo! One-to-many flow and channel support

--- flowdock-irc.rb
+++ flowdock-irc.rb
@@ -6,12 +6,23 @@ require 'net/https'
 require 'json'
 require 'isaac/bot'
 
+# Personal API token to get flow info and for API access
+# Get it from https://www.flowdock.com/account/tokens
 PERSONAL_TOKEN = ""
-FLOW = "your-flow"
-ORGANIZATION = "your-organization"
+
+# Route configuration
+# One to many mappings are also possible with 
+# 'org/flow' => ['#chan1', '#chan2']
+FLOW_TO_IRC = {
+  "org/flow" => "#flowdock-bot"
+}
+IRC_TO_FLOW = {
+  "#flowdock-bot" => "org/flow"
+}
+
+# IRC bot configuration
 IRC_NICK = "flowbotti#{rand(100).to_s}"
 IRC_SERVER = "irc.stealth.net"
-IRC_CHANNEL = "#flowdock-bot" #channel [password]
 IRC_REALNAME = "Flowdock IRC Bot"
 
 class FlowDockBot < Isaac::Bot
@@ -19,15 +30,20 @@ class FlowDockBot < Isaac::Bot
 end
 
 class FlowdockIRC
-  # Personal API token to get flow info and for API access
-  # Get it from https://www.flowdock.com/account/tokens
-  attr_accessor :bot, :flow, :latest_messages
+  attr_accessor :bot, :flows, :latest_messages
 
   def initialize
     puts "Starting Flowdock IRC Bot"
     @latest_messages = []
+    # Init IRC bot
     init_bot
-    load_flow_info
+    # Load flow info to get id -> nick mappings
+    @flows = {}
+    puts "Loading flow info..."
+    FLOW_TO_IRC.keys.each do |flow|
+      puts "- #{flow}"
+      @flows[flow] = load_flow_info(flow)
+    end
   end
 
   def init_bot
@@ -39,8 +55,12 @@ class FlowdockIRC
         c.realname = IRC_REALNAME
       end
       on :connect do
-        puts "Connected to IRC!"
-        join IRC_CHANNEL
+        puts "Connected to #{IRC_SERVER}"
+        puts "Joining to channels..."
+        @gateway.all_channels.each do |channel|
+          puts "- #{channel}"
+          join channel
+        end
       end
       on :channel, // do
         # We want to store few latest message sent to flowdock
@@ -49,8 +69,10 @@ class FlowdockIRC
         while @gateway.latest_messages.size > 10 do
           @gateway.latest_messages.pop(0)
         end
-        # Send message to flow
-        @gateway.send_to_flowdock(nick, message)
+        # Send message to flows
+        @gateway.flow_targets_for(channel).each do |flow|
+          @gateway.send_to_flowdock(flow, nick, message)
+        end
       end
       on :error do
         # TODO: Error handling
@@ -60,18 +82,37 @@ class FlowdockIRC
     @bot.gateway = self
   end
 
-  def id_to_nick(id)
-    user = @flow['users'].select{|user| user['id'].to_s == id.to_s}.first
+  def all_channels
+    (FLOW_TO_IRC.values + IRC_TO_FLOW.keys).flatten.uniq
+  end
+
+  def id_to_nick(flow, id)
+    flow = flow.gsub(':', '/')
+    return id unless @flows[flow]
+    user = @flows[flow]['users'].select{|user| user['id'].to_s == id.to_s}.first
     user ? user['nick'] : id
   end
 
-  def send_to_irc(nick, message)
-    # Split possible password out from channel name
-    @bot.msg(IRC_CHANNEL.split(' ').first, "<#{nick}> #{message}")
+  def load_flow_info(flow)
+    uri = URI("https://api.flowdock.com/flows/#{flow}")
+    http = Net::HTTP.new(uri.host, 443)
+    http.use_ssl = true
+    http.start() do |http|
+      req = Net::HTTP::Get.new(uri.request_uri)
+      req.basic_auth PERSONAL_TOKEN, ''
+      res = http.request(req)
+      raise "Could not load flow info. Invalid PERSONAL_TOKEN?" if res.code != '200'
+      @flow = JSON.parse(res.body)
+    end
   end
 
-  def send_to_flowdock(nick, message)
-    uri = URI("https://api.flowdock.com/flows/#{ORGANIZATION}/#{FLOW}/messages")
+  def flow_targets_for(channel)
+    return IRC_TO_FLOW[channel].to_a if IRC_TO_FLOW[channel]
+    []
+  end
+
+  def send_to_flowdock(flow, nick, message)
+    uri = URI("https://api.flowdock.com/flows/#{flow}/messages")
     http = Net::HTTP.new(uri.host, 443)
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     http.use_ssl = true
@@ -91,18 +132,15 @@ class FlowdockIRC
     end
   end
 
-  def load_flow_info
-    puts "Loading flow info..."
-    uri = URI("https://api.flowdock.com/flows/#{ORGANIZATION}/#{FLOW}")
-    http = Net::HTTP.new(uri.host, 443)
-    http.use_ssl = true
-    http.start() do |http|
-      req = Net::HTTP::Get.new(uri.request_uri)
-      req.basic_auth PERSONAL_TOKEN, ''
-      res = http.request(req)
-      raise "Could not load flow info. Invalid PERSONAL_TOKEN?" if res.code != '200'
-      @flow = JSON.parse(res.body)
-    end
+  def irc_targets_for(flow)
+    flow = flow.gsub(':', '/')
+    return FLOW_TO_IRC[flow].to_a if FLOW_TO_IRC[flow]
+    []
+  end
+
+  def send_to_irc(channel, nick, message)
+    # Split possible password out from channel name
+    @bot.msg(channel.split(' ').first, "<#{nick}> #{message}")
   end
 
   def sent_from_irc?(data)
@@ -112,12 +150,13 @@ class FlowdockIRC
   end
 
   def flowdock_stream
-    http = EM::HttpRequest.new("https://stream.flowdock.com/flows/#{ORGANIZATION}/#{FLOW}").get(
+    http = EM::HttpRequest.new("https://stream.flowdock.com/flows/?filter=#{FLOW_TO_IRC.keys.join(',')}").get(
       :head => {'Authorization' => [PERSONAL_TOKEN, ''], 'Accept' => 'text/json'}, 
       :keepalive => true, 
       :connect_timeout => 0, 
       :inactivity_timeout => 0
     )
+    # FIXME: This occurs somehow always after 10s idle
     http.errback {|err| EM.stop}
     buffer = "" 
     http.stream do |chunk|
@@ -126,7 +165,10 @@ class FlowdockIRC
         data = JSON.parse(line)
         # We want only messages and not the ones sent from IRC itself
         if data['event'] == "message" and !sent_from_irc?(data)
-          send_to_irc(id_to_nick(data['user']), data['content'])
+          puts data.inspect
+          irc_targets_for(data['flow']).each do |channel|
+            send_to_irc(channel, id_to_nick(data['flow'], data['user']), data['content'])
+          end
         end
       end
     end
